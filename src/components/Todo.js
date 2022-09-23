75f3b3c08429e83392ce68b44faf3cd8a468118d
Carlo Lopez <carlolb1232@gmail.com>
2022-09-23 02:25:28 -0500

    First commit - ejercicio resuelto

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+export function ToDo() {
+  return(
+    <>
+      <h2>Things I need to do</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  )
+}
+
+// export default ToDo;
+
+
